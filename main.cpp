399258e64ad62576d347a922d1bbc3b5d6656693
Briand84 <brianmdignam@hotmail.com>
2023-08-05 18:40:40 +0000

    Commit 2_ Recursive Function
    Signed-off-by: Briand84 <brianmdignam@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
 //Brian Dignam Lab 2
 #include "mbed.h"
 
-int main(){//runs its own thread in the operating system
+//n = 1 ..... 0
+//n = 2 ..... 1
+//n = 3 ..... 1
+//n = 4 ..... 2
 
-printf("Brian Dignam Lab 2\r\n");
- //Declare Variables that are integers
-int max_term = 10;
-int term_1 = 0;
-int term_2= 1;
-int next_term = 0;
-        for(int i=1; i<=max_term; i++){ //Continious loop whilst i is less then Max term
-if (i == 1)
-    printf("%d\r\n", term_1); //prints first term
- if (i == 2)
-       printf("%d\r\n",term_2); //prints second term
-else{
- next_term = term_1 + term_2;
- term_1 = term_2;
- term_2 = next_term;
-       printf("%d\r\n", next_term); //result printed on tera-term
- }
+int fib(int n){
+    if (n == 1)
+    return 0;
+    if (n == 2)
+    return 1;
+    else
+    return (fib(n-1)+fib(n-2));
+
+//3rd ....(1+0)=1=1
+//4th ....f(3)+f(2)....(1+1)=2
+// continues till max term is reached     
 }
-      printf("\r\n");
 
-      while (true){
+int main()
+{
+printf("Lab 2_part 2 - Recursive Function\r\n");
+       int max_term = 10;
 
-      
-      }
+    for(int i=1; i<=max_term; i++){
+        printf("%d\r\n", fib(i));
+}   
+printf("\r\n");
 }
